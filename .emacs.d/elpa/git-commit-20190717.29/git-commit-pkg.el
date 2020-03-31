069fe2be7c3fafe0974b9d0ca5691554fcbc4158
Valerio Besozzi <valebes@pop-os.localdomain>
2020-03-31 12:51:39 +0200

    Clean repo

--- .emacs.d/elpa/git-commit-20190717.29/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20190717.29" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "546a9c33715f12a9b1101eca22b070004c9c6392" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
